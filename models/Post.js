45c28263b9fc1c8f770fa46b9ce186d9f760973b
Chris <chris.feight@gmail.com>
2021-12-04 21:25:59 -0800

    posts model

---
+++ models/Post.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    title: {
+        type: DataTypes.STRING, 
+        allowNull: false,
+    },
+  
+
+},{
+    sequelize
+});
+
+module.exports = Post
