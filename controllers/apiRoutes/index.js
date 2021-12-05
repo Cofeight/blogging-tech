9d01b8d0d2018ed7bba00aa0f8d395719fce7603
Chris <chris.feight@gmail.com>
2021-12-04 21:45:25 -0800

    apirouting established

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const apiRoutes = require('./apiRoutes');
 
-router.use('/api', apiRoutes)
 router.get('/',(req, res) => {
-    res.send("HI FROM CONT/INDEX!")
+    res.send("Halloski API CALLING")
 })
 
 module.exports = router;
